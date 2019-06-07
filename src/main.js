f1325c61f75d5bd51e10ddf84a023f2040b7ddde
jk <kerntzj@gmail.com>
2019-06-07 10:57:02 -0700

    add api search dom test, kingButton added to index.html

--- src/main.js
+++ src/main.js
@@ -1,9 +1,17 @@
 import $ from 'jquery';
+import { Doctor } from './betterDoctor.js';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
 
 $(document).ready(function(){
-  let key = process.env.exports.apikey;
-  console.log(key);
+  let test = new Doctor();
+  console.log(test);
+  $("#kingButton").on("click", () => {
+    let value = "Toothache";
+    setTimeout(()=>{
+      let results = test.findMD(value);
+      // $("#doctor").append(`${body}`);
+    }, 5000);
+  })
 });
